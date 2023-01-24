3ffe05f21420a933f33de54f56c7f79d12148494
TomtenPim <simon.agren@elev.ga.ntig.se>
2023-01-24 14:27:22 +0100

    Del 7 css

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3000;
-
 const indexRouter = require('./routes/index');
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 nunjucks.configure('views', {
     autoescape: true,
--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,18 @@ router.get('/', async function (req, res) {
     });
 });
 
+router.get('/new', async function (req, res, next) {
+    res.render('new.njk', {
+        title: 'Nytt inlägg',
+    });
+});
+
+router.post('/new', async function (req, res, next) {
+    const { title, content } = req.body;
+    const author = 1;
+    const [rows] = await promisePool.query("INSERT INTO sa04forum (author, title, content) VALUES (?, ?, ?)", [author, title, content]);
+    res.redirect('/');
+});
 
 
 module.exports = router;
